1d2545133515e0c30cd5756cba640d07453d4cd7
Xyrec <itsgonnabealongnight@gmail.com>
2018-05-22 17:57:43 +0200

    Add files via upload

---
+++ 4chan-cdn-fixer.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name                        4chan.org CDN Fixer
+// @description                 Reverts is2.4chan.org CDN to the one at i.4cdn.org
+// @author                      Xyrec
+// @version                     1.0
+// @include                     *.4chan.org/*
+// @require                     https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// ==/UserScript==
+
+$(function(){
+    $("a").each(function() {
+        this.href = this.href.replace('is2.4chan.org' , 'i.4cdn.org');
+    });
+});
