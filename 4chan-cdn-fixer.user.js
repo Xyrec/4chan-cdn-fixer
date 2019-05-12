cc1563b393ab364bbebbca4b446d7d978fd3bcff
Xyrec <itsgonnabealongnight@gmail.com>
2019-05-12 20:36:41 +0200

    Fixed mistake

--- 4chan-cdn-fixer.user.js
+++ 4chan-cdn-fixer.user.js
@@ -2,17 +2,18 @@
 // @name                        4chan.org CDN Fixer
 // @description                 Reverts is2.4chan.org CDN to the one at i.4cdn.org
 // @author                      Xyrec
-// @version                     1.1.1
-// @updateURL                   https://raw.githubusercontent.com/Xyrec/4chan-cdn-fixer/master/4chan-cdn-fixer.user.js
+// @version                     1.1
 // @include                     *.4chan.org/*
 // @include                     *.4channel.org/*
 // @require                     https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @updateURL                   https://raw.githubusercontent.com/Xyrec/4chan-cdn-fixer/master/4chan-cdn-fixer.user.js
 // ==/UserScript==
 
 var imgLinks = document.querySelectorAll('a.fileThumb');
 for (var _i = imgLinks.length - 1; _i >= 0; _i--)
 {
 var imgLink = imgLinks[_i];
-if (imgLink.href.match(/is\d\.4chan\.org/))
+if (imgLink.href.match(/is\d\.4chan\.org/)) {
 imgLink.href = imgLink.href.replace(/is\d\.4chan\.org/, 'i.4cdn.org');
+}
 }
