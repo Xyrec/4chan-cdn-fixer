884b34a43e623c020767ca1b83c3d1fd8e01c818
Xyrec <itsgonnabealongnight@gmail.com>
2019-05-12 20:46:02 +0200

    Fixed URL links

--- 4chan-cdn-fixer.user.js
+++ 4chan-cdn-fixer.user.js
@@ -2,18 +2,25 @@
 // @name                        4chan.org CDN Fixer
 // @description                 Reverts is2.4chan.org CDN to the one at i.4cdn.org
 // @author                      Xyrec
-// @version                     1.2
+// @version                     1.3
 // @include                     *.4chan.org/*
 // @include                     *.4channel.org/*
 // @require                     https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @updateURL                   https://raw.githubusercontent.com/Xyrec/4chan-cdn-fixer/master/4chan-cdn-fixer.user.js
 // ==/UserScript==
 
-var imgLinks = document.querySelectorAll('a.fileThumb');
-for (var _i = imgLinks.length - 1; _i >= 0; _i--)
-{
-var imgLink = imgLinks[_i];
-if (imgLink.href.match(/is\d\.4chan\.org/)) {
-imgLink.href = imgLink.href.replace(/is\d\.4chan\.org/, 'i.4cdn.org');
-}
-}
+var thumbLinks = document.querySelectorAll('a.fileThumb');
+	for (var _i = thumbLinks.length - 1; _i >= 0; _i--) {
+	var thumbLink = thumbLinks[_i];
+		if (thumbLink.href.match(/is\d\.4chan\.org/)) {
+		thumbLink.href = thumbLink.href.replace(/is\d\.4chan\.org/, 'i.4cdn.org');
+		}
+	}
+
+var urlLinks = document.querySelectorAll('div.fileText > a');
+	for (var i = urlLinks.length - 1; i >= 0; i--) {
+	var urlLink = urlLinks[i];
+		if (urlLink.href.match(/is\d\.4chan\.org/)) {
+		urlLink.href = urlLink.href.replace(/is\d\.4chan\.org/, 'i.4cdn.org');
+		}
+	}
